a45ce858e349aecee5201243443c1d765276cbff
fabioP <faparedes28@hotmail.com>
2024-05-06 16:06:15 -0500

    modificado

---
+++ PrGithub/PracticaEscritura/Escritura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaEscritura
+{
+    public class Escritura
+    {
+        public void Escribir() 
+        {
+
+            Console.WriteLine("Holaaaaaaa");
+            Console.ReadKey();
+        
+        }
+    }
+}
